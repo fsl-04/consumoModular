176d2bea050502bc42b8b54d1cca4cef09a8cc49
ClovisDuarte <csduarte.souza@gmail.com>
2024-09-23 21:52:07 -0300

    Inserindo mapeamento carrinhos

--- js/mostrarProdutos.js
+++ js/mostrarProdutos.js
@@ -1,5 +1,15 @@
 import { listaProdutos } from "./chamaApi";
 
+const lista = document.querySelector('.produtos');
+
 function exibirProdutos() {
     const listaApi = listaProdutos();
+
+listaApi.carts.forEach(carrinho => {
+    carrinho.carts.forEach(produto => {
+        lista.appendChild(
+            constroeCard()
+        );
+    })    
+});    
 }
